678361d2e3419af2f7f8e0e41eae76414d2402ca
Leo <maxinyu@pku.edu.cn>
2018-03-11 21:46:07 +0800

    2

--- app/src/main/java/com/arthur/myapplication/TaskAdapter.java
+++ app/src/main/java/com/arthur/myapplication/TaskAdapter.java
@@ -4,11 +4,13 @@ package com.arthur.myapplication;
  * Created by Leo on 2018/3/3.
  */
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import java.util.List;
 import java.util.zip.Inflater;
@@ -22,12 +24,21 @@ public class TaskAdapter extends ArrayAdapter<Task_Lists>{
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        Task_Lists task = getItem(position);
+        final Task_Lists task = getItem(position);
         View OneTaskView = LayoutInflater.from(getContext()).inflate(R.layout.task_item, parent,false);
         TextView textView = (TextView) OneTaskView.findViewById(R.id.task_name_view);
         TextView subtextView = (TextView) OneTaskView.findViewById(R.id.task_desc_view);
         textView.setText(task.GetName());
         subtextView.setText(task.GetDesc());
+        OneTaskView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), TaskDetailActivity.class);
+                intent.putExtra("task_name", task.GetName());
+                intent.putExtra("task_desc", task.GetDesc());
+                getContext().startActivity(intent);
+            }
+        });
 
         return OneTaskView;
 
--- app/src/main/java/com/arthur/myapplication/TaskDetailActivity.java
+++ app/src/main/java/com/arthur/myapplication/TaskDetailActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class TaskDetailActivity extends AppCompatActivity {
 
@@ -15,6 +16,14 @@ public class TaskDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String name = getIntent().getStringExtra("task_name");
+        String desc = getIntent().getStringExtra("task_desc");
+        Toolbar nameView = (Toolbar) findViewById(R.id.TaskDetail);
+        setSupportActionBar(nameView);
+        TextView descView = (TextView) findViewById(R.id.TaskDetailDesc);
+        nameView.setTitle(name);
+        descView.setText(desc);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
